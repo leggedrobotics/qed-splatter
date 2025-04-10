b5d6b561b5132173baae0517c8053a0235052c49
Jannick Schröer <43840463+IQisMySenpai@users.noreply.github.com>
2025-04-10 17:54:51 +0200

    5 change of the depth unit scale (#6)
    * Update of the depth_unit_scale to millimeters
    * Update README.md

--- qed_splatter/dataparser.py
+++ qed_splatter/dataparser.py
@@ -8,7 +8,7 @@ from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataPars
 @dataclass
 class QEDSplatterDataParserConfig(NerfstudioDataParserConfig):
     _target: Type = field(default_factory=lambda: QEDSplatterDataParser)
-    depth_unit_scale_factor: float = 1
+    depth_unit_scale_factor: float = 0.001
     auto_scale_poses: bool = False  # If True, poses will be scaled to max extent of 1
     center_method: Literal["poses", "focus", "none"] = "none"  # Centering method
     orientation_method: Literal["pca", "up", "vertical", "none"] = "none"  # Orientation method
